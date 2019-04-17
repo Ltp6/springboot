f98ec20bc2c6c954f95f72af6781305f076dfcc0
Ltp <18502825783@163.com>
2019-04-17 22:11:12 +0800

    commit

--- src/main/java/com/ltp/config/MyMvcConfig.java
+++ src/main/java/com/ltp/config/MyMvcConfig.java
@@ -1,6 +1,7 @@
 package com.ltp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Version 1.0
  */
 @Configuration
+//使SpringMVC的自动配置文件失效
+@EnableWebMvc
 public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
